1694b4391d976a6ebba590f136f484be4f424078
p33dro <133718489+Pedro867@users.noreply.github.com>
2024-01-11 20:47:21 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,49 @@
+const questions = [
+    {
+        question: "Em qual ano William Shakespeare nasceu?",
+        answers: [
+            { option: "1497", correct: false},
+            { option: "1564", correct: true},
+            { option: "1584", correct: false},
+            { option: "1603", correct: false},
+        ],
+    },
+    {
+        question: "Qual é a peça romântica trágica de Shakespeare?",
+        answers: [
+            { option: "A Megera Domada", correct: false},
+            { option: "Macbeth", correct: false},
+            { option: "Romeu e Julieta", correct: true},
+            { option: "Rei Lear", correct: false},
+        ],
+    },
+    {
+        question: "Quantos sonetos o autor possui?",
+        answers: [
+            { option: "154", correct: true},
+            { option: "38", correct: false},
+            { option: "197", correct: false},
+            { option: "74", correct: false},
+        ],
+    },
+    {
+        question: "Qual peça do dramaturgo trata do tema da corrupção e traição?",
+        answers: [
+            { option: "Romeu e Julieta", correct: false},
+            { option: "Sonho de uma Noite de Verão", correct: true},
+            { option: "Hamlet", correct: true},
+            { option: "O Mercador de Veneza", correct: false},
+        ],
+    },
+    {
+        question: "Qual a nacionalidade de Shakespeare?",
+        answers: [
+            { option: "Inglês", correct: true},
+            { option: "Americano", correct: false},
+            { option: "Escocês", correct: false},
+            { option: "Italiano", correct: false},
+        ],
+    },
+];
+
+export default questions;
--- script.js
+++ script.js
@@ -1,16 +1,94 @@
-const options = document.querySelectorAll('.opção');
+const questionContainer = document.querySelector(".question");
+const answerContainer = document.querySelector(".options");
+const spnQtd = document.querySelector(".questionNumber");
+const textFinish = document.querySelector(".finish span");
+const content = document.querySelector(".content");
+const contentFinish = document.querySelector(".finish");
+const btnRestart = document.querySelector(".finish button");
+const contentStart = document.querySelector(".start");
+const btnStart = document.querySelector(".start button");
+const contentResposta = document.querySelector(".resposta");
+const btnResposta = document.querySelector(".respBtn");
 
-options.forEach(opção => {
-    opção.addEventListener('click', function() {
-        const isCorrect = this.getAttribute('data-correct') === 'true';
-        var other = document.getElementsByClassName('errada');
+import questions from "./questions.js";
 
-        if (isCorrect) {
-            this.classList.add('correta');
-        }
-        else {
-            this.classList.add('errada');
-        }
+let currentQuestion = 0;
+let correctQuestions = 0;
+
+btnStart.onclick = () => {
+    content.style.display = "flex";
+    contentStart.style.display = "none";
+
+    currentQuestion = 0;
+    correctQuestions = 0;
+    loadQuestion();
+};
+
+function start (){
+    content.style.display = "none";
+    contentStart.style.display = "flex";
+}
+
+btnResposta.onclick = () => {
+    btnResposta.style.display = "none"; 
+    contentResposta.style.display = "flex";
+}
+
+btnRestart.onclick = () => {
+    content.style.display = "flex";
+    contentFinish.style.display = "none";
+    contentResposta.style.display = "none";
+    btnResposta.style.display = "none";
+    contentResposta.innerHTML = ""
+
+    currentQuestion = 0;
+    correctQuestions = 0;
+    loadQuestion();
+};
+
+function nextQuestion(e) {
+
+    let userAnswer = e.target.getAttribute("data-correct");
+
+    if (userAnswer === "true") {
+        correctQuestions++;
+    }
+
+    if (currentQuestion < questions.length - 1) {
+        currentQuestion++;
+        loadQuestion();
+    } else {
+        finish();
+    }
+}
+
+function finish() {
+    textFinish.innerHTML = `Você acertou ${correctQuestions} de ${questions.length}`;
+    content.style.display = "none";
+    contentFinish.style.display = "flex";
+    btnResposta.style.display = "flex";
+}
+
+function loadQuestion() {
+    spnQtd.innerHTML = `${currentQuestion + 1}/${questions.length}`;
+    const currentQuestionItem = questions[currentQuestion];
+    answerContainer.innerHTML = "";
+    questionContainer.innerHTML = currentQuestionItem.question;
+
+    currentQuestionItem.answers.forEach((answer) => {
+        const div = document.createElement("div");
+
+        div.innerHTML = `
+        <button class="options" data-correct="${answer.correct}">
+            ${answer.option}
+        </button>
+        `;
+
+        answerContainer.appendChild(div);
+
+        div.querySelector(".options").addEventListener("click", nextQuestion);
     });
-});
+}
 
+loadQuestion();
+start();
